d4dc05645bf963ad81d729bcdc83c5e40cf352cb
VictorPrado99 <prado.young@gmail.com>
2020-12-13 01:34:31 -0300

    Adição das Entidades
    waf

--- models/pedido.py
+++ models/pedido.py
@@ -3,3 +3,15 @@ class Pedido:
 
     def __init__(self):
         pass
+
+    def getIdPedido(self):
+        return self.__IdPedido
+
+    def setIdPedido(self, IdPedido):
+        self.__IdPedido = IdPedido
+
+    def getNfe(self):
+        return self.__Nfe
+
+    def setNfe(self, Nfe):
+        self.__Nfe = Nfe
--- models/transporte.py
+++ models/transporte.py
@@ -3,3 +3,21 @@ class Transporte:
 
     def __init__(self):
         pass
+
+    def getIdTransporte(self):
+        return self.__IdTransporte
+
+    def setIdTransporte(self, IdTransporte):
+        self.__IdTransporte = IdTransporte
+
+    def getIdentificacao(self):
+        return self.__Identificacao
+
+    def setIdentificacao(self, Identificacao):
+        self.__Identificacao = Identificacao
+
+    def getIdTipoTransporte(self):
+        return self.__IdTipoTransporte
+
+    def setIdTipoTransporte(self, IdTipoTransporte):
+        self.__IdTipoTransporte = IdTipoTransporte
