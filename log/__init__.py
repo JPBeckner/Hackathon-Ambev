6ed6b8b73149b728bab840b5b7f5e43b4d7b618d
JPBeckner <pedrobeckner@hotmail.com>
2020-12-13 02:22:17 -0300

    fix log

--- log/__init__.py
+++ log/__init__.py
@@ -1,23 +1,30 @@
 import logging
 from os.path import abspath, dirname
+from logging.config import dictConfig
 
 # file_path = dirname(__name__)
 file_path = abspath(__name__)
 
-LOGGING = {
+DEFAULT_LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'filename': f'{file_path}/server.log'
+    'formatters': {
+        'verbose': {
+            'format': '{asctime} - [{module}] - {levelname}: {message}',  # {process:d} {thread:d}
+            'style': '{',
         },
+    },
+    'handlers': {
         'file': {
-            'level': 'DEBUG',
             'class': 'logging.FileHandler',
             'filename': f'{file_path}/server.log',
-        }
+            'formatter': 'verbose'
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+
     },
     'root': {
         'handlers': ['console'],
@@ -37,8 +44,10 @@ class Logger:
 
     def __init__(self, name: str = 'server'):
         self.__name = name
-
-        self.__logger = logging.LoggerAdapter(logging.getLogger(self.__name), extra={'server': 'teste'})
+        dictConfig(DEFAULT_LOGGING)
+        self.__logger = logging.LoggerAdapter(logging.getLogger(name), extra={'server': 'teste'})
+        # self.__logger.setLevel('DEBUG')
+        # self.__logger.f
 
     @property
     def logger(self) -> logging.LoggerAdapter:
